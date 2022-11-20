9f54075b1a16887a2acd9e8d7d044bb733316c07
Nedoxy <nk.nedum@gmail.com>
2022-11-20 20:41:48 +0100

    Update

--- JavaScript-Programming-Language/JavaScript_Challenge/js_code_challenge8.js
+++ JavaScript-Programming-Language/JavaScript_Challenge/js_code_challenge8.js
@@ -1,29 +1,11 @@
 
 //Return a Boolean if a number is divisible by 10
 
-let num = 40;
-let result = num % 10 == 0 ? true : false;
-console.log(result);
-
-console.log('=== OR ===');
-
-let i = 35;
-if (i % 10 == 0) {
-    console.log(true);
-} else {
-    console.log(false);
-}
-
-console.log('=== OR ===');
-
-const number = function() {
-    let num = newNum();
-    let testResult = num % 10;
-    if(testResult == 0){
-        console.log(true);
-    } else {
-        console.log(false);
+function divisible(nos){
+    if (nos % 10 === 0) {
+        return true
+    }else {
+        return false
     }
 }
-
-newNum(20);
+console.log(divisible(75))
